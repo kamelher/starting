6f7dcabb8879921a74941063c2d430eca5d81088
kamel heragmi <kamel.heragmi@gmail.com>
2023-04-22 14:02:26 +0100

    rrefactor myFunctions.js

--- public/js/app.js
+++ public/js/app.js
@@ -78,9 +78,7 @@ function readURL(input) {
   if (input.files && input.files[0]) {
     var reader = new FileReader();
     reader.onload = function (e) {
-      $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
-      $('#imagePreview').hide();
-      $('#imagePreview').fadeIn(650);
+      $('#imagePreview').css('background-image', 'url(' + e.target.result + ')').hide().fadeIn(650);
     };
     reader.readAsDataURL(input.files[0]);
   }
